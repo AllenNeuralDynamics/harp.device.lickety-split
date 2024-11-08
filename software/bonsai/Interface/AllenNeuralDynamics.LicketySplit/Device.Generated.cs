6d240f59f1fc0ded6c7aef273d6a5d4a9d292442
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-11-08 12:45:41 -0800

    Update bonsai generators

--- software/bonsai/Interface/AllenNeuralDynamics.LicketySplit/Device.Generated.cs
+++ software/bonsai/Interface/AllenNeuralDynamics.LicketySplit/Device.Generated.cs
@@ -41,6 +41,41 @@ namespace AllenNeuralDynamics.LicketySplit
             { 33, typeof(Channel0TriggerThreshold) },
             { 34, typeof(Channel0UntriggerThreshold) }
         };
+
+        /// <summary>
+        /// Gets the contents of the metadata file describing the <see cref="LicketySplit"/>
+        /// device registers.
+        /// </summary>
+        public static readonly string Metadata = GetDeviceMetadata();
+
+        static string GetDeviceMetadata()
+        {
+            var deviceType = typeof(Device);
+            using var metadataStream = deviceType.Assembly.GetManifestResourceStream($"{deviceType.Namespace}.device.yml");
+            using var streamReader = new System.IO.StreamReader(metadataStream);
+            return streamReader.ReadToEnd();
+        }
+    }
+
+    /// <summary>
+    /// Represents an operator that returns the contents of the metadata file
+    /// describing the <see cref="LicketySplit"/> device registers.
+    /// </summary>
+    [Description("Returns the contents of the metadata file describing the LicketySplit device registers.")]
+    public partial class GetMetadata : Source<string>
+    {
+        /// <summary>
+        /// Returns an observable sequence with the contents of the metadata file
+        /// describing the <see cref="LicketySplit"/> device registers.
+        /// </summary>
+        /// <returns>
+        /// A sequence with a single <see cref="string"/> object representing the
+        /// contents of the metadata file.
+        /// </returns>
+        public override IObservable<string> Generate()
+        {
+            return Observable.Return(Device.Metadata);
+        }
     }
 
     /// <summary>
