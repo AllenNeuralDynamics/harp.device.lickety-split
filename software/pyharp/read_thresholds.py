2cefb12fa24c8b293535d39baae771a34df188b1
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2026-08-03 19:19:45 -0700

    update pyharp examples

--- software/pyharp/read_thresholds.py
+++ software/pyharp/read_thresholds.py
@@ -1,28 +1,23 @@
-#!/usr/bin/env python3
-from pyharp.device import Device, DeviceMode
-from pyharp.messages import HarpMessage
-from pyharp.messages import MessageType
-from pyharp.messages import CommonRegisters as Regs
-from struct import *
-import os
-from time import sleep, perf_counter
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "harp>=0.5.0rc1",
+# ]
+#
+# [tool.uv]
+# prerelease = "allow"
+# ///
 
+from harp.device import Device, WhoAmI
+from harp.serial import open_serial_device
+from lickety_split import Channel0TriggerThreshold, Channel0UntriggerThreshold
 
-# Open the device and print the info on screen
-# Open serial connection and save communication to a file
-if os.name == 'posix': # check for Linux.
-    #device = Device("/dev/harp_device_00", "ibl.bin")
-    device = Device("/dev/ttyACM0", "ibl.bin")
-else: # assume Windows.
-    device = Device("COM95", "ibl.bin")
+COM_PORT = "/dev/ttyACM0"
 
-# Read encoder and torque raw measurements.
-try:
-        on_threshold = device.send(HarpMessage.ReadU8(33).frame)
-        off_threshold = device.send(HarpMessage.ReadU8(34).frame)
-        print(f"on threshold: {on_threshold}")
-        print(f"off_threshold: {off_threshold}")
-        print()
-finally:
-    # Close connection
-    device.disconnect()
+# Read raw thresholds.
+with open_serial_device(Device, port=COM_PORT, baudrate=1_000_000) as dev:
+    print(dev.read(WhoAmI).parsed)
+    trigger_threshold = dev.read(Channel0TriggerThreshold).parsed
+    untrigger_threshold = dev.read(Channel0UntriggerThreshold).parsed
+    print(f"Trigger threshold: {trigger_threshold}, "
+          f"Untrigger_threshold: {untrigger_threshold}")
